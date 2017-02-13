8276a2ff5e78b0bc23ead200769457bff1922785
Atsushi Ohta <qqep685d@gmail.com>
2017-02-13 23:27:18 +0900

    first commit

---
+++ fasta/ExtractSeq_by_ID.py
@@ -0,0 +1,25 @@
+"""
+# Usage: python this_program.py -i input.fasta [-o output.fasta -w "index word" -m "c"]
+"""
+
+import sys
+import argparse
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="Input fasta file")
+parser.add_argument("-o", "--output", help="Output fasta file", default=sys.stdout)
+parser.add_argument("-w", "--word", help="Index word for sequence id", default=None)
+parser.add_argument("-m", "--match", help="Complete match (c: default) or partial match (p)", default='c')
+args = parser.parse_args()
+
+seqs = []
+for seq in SeqIO.parse(args.input, 'fasta'):
+	if args.match == 'p':
+		if  args.word in seq.id:
+			seqs.append(seq)
+	else:
+		if  args.word == seq.id:
+			seqs.append(seq)
+
+SeqIO.write(seqs, args.output, "fasta")
